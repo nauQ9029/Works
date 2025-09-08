9c01f006465b09401841024bcb127fff6fea3e48
nauQ <plminhquan@gmail.com>
2025-09-08 18:40:18 +0700

    Commit

---
+++ FUO/PaperNo_1/PaperNo_1/All/Resource/js/mycode.js
@@ -0,0 +1,9 @@
+function openNav(){
+	var x = document.getElementById('mySidenav');
+	x.style.width="100%";
+}
+
+function closeNav(){
+	var x = document.getElementById('mySidenav');
+	x.style.width="0";
+}
