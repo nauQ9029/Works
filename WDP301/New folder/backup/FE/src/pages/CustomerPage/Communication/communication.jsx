25f1bb9245c03a356f404b78470e7cc738d5b16b
nauQ <plminhquan@gmail.com>
2025-11-12 15:18:08 +0700

    FixGo

--- WDP301/New folder/backup/FE/src/pages/CustomerPage/Communication/communication.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Communication from "../../CommonPage/Communication/Communication";
-
-export default function CustomerCommunication() {
-  return <Communication role="customer" />;
-}
