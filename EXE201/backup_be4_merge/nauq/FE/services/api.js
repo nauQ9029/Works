0eaa3179e0896f8cf68788114b0479e57d7ba9a3
nauQ <155826493+nauQ9029@users.noreply.github.com>
2025-11-12 15:21:53 +0700

    Delete EXE201 directory

--- EXE201/backup_be4_merge/nauq/FE/services/api.js
+++
@@ -1,2 +0,0 @@
-const BASE_URL = "http://192.168.106.184:5000/api"; // IP của quân
-export default BASE_URL;
--- EXE201/backup_be4_merge/panh_luan/BE/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-
-router.get('/all', userController.getAllUsers);
-router.get('/:userId', userController.getUserById);
-
-module.exports = router;
